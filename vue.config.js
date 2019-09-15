a4927967d76cc3e4353b641c6c5963503ee53eee
SuleymanDTR <slymnd28@gmail.com>
2019-09-16 01:41:48 +0300

    import scss files globally

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+	css: {
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "@/assets/scss/_mixins.scss";
+        `
+      }
+    }
+  }
 }
