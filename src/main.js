52eb6feabbdedf9dab537abf0a4b6836b6b349a5
SuleymanDTR <slymnd28@gmail.com>
2019-09-15 22:02:36 +0300

    add axios, bootstrap, swiper

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap-reboot.css'
+import 'bootstrap/dist/css/bootstrap-grid.css'
+
+// Main SCSS
+import '@/assets/scss/main.scss'
+
+// Axios (for http requests)
+const axiosConfig = axios.create({
+  baseURL: process.env.VUE_APP_API_ENDPOINT
+})
+Vue.prototype.$http = axiosConfig
 
 Vue.config.productionTip = false
 
