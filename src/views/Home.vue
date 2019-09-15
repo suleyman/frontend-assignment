b4d1abe97d4aff9da6339899e6b6f5aa961da694
SuleymanDTR <slymnd28@gmail.com>
2019-09-16 01:43:03 +0300

    fetch news & pass data to components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,46 @@
 <template>
   <div class="home">
-
+		<div class="container mb-5">
+			<!-- Öne çıkan haberler -->
+			<h2 class="section-title mt-4">Öne Çıkan Haberler</h2>
+			<FeaturedNews class="mt-4" :featured-news="featuredNews" />
+			<!-- Diğer haberler -->
+			<h2 class="section-title mt-4">Son Dakika</h2>
+			<News class="mt-0 mt-lg-4" :news="nonFeaturedNews" />
+			<News class="mt-0 mt-lg-4" :news="nonFeaturedNews" />
+			<News class="mt-0 mt-lg-4" :news="nonFeaturedNews" />
+		</div>
   </div>
 </template>
 
 <script>
+import FeaturedNews from '@/components/Home/FeaturedNews.vue'
+import News from '@/components/Home/News.vue'
 export default {
-  name: 'home',
   components: {
-
-  }
+		FeaturedNews,
+		News
+	},
+	data() {
+		return {
+			news: []
+		}
+	},
+  mounted() {
+		// tüm haberleri getir
+		this.$http.get('/news').then(response => {
+			this.news = response.data;
+		})
+	},
+	computed: {
+		// isFeatured = true olan haberleri filtreleyen computed property
+		featuredNews() {
+			return this.news.filter(newsItem => newsItem.isFeatured)
+		},
+		// isFeatured = false olan haberleri filtreleyen computed property
+		nonFeaturedNews() {
+			return this.news.filter(newsItem => !newsItem.isFeatured)
+		}
+	}
 }
 </script>
