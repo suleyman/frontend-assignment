f76f565955328f6da58494b5c1d6aec71b117e1e
SuleymanDTR <slymnd28@gmail.com>
2019-09-16 01:43:20 +0300

    added header compontent

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="container">
+		<header class="header">
+				<router-link :to="{ name: 'home' }" class="header__logo">
+					Frontend Assignment | News
+				</router-link>
+		</header>
+	</div>
+</template>
